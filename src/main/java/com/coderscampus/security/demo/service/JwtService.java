7b149686b0a87c797f01343601eef38fdef6eec6
tp02ga <trevor@coderscampus.com>
2023-07-05 09:32:40 -0400

    14 - Creating a JwtService Part 2

--- src/main/java/com/coderscampus/security/demo/service/JwtService.java
+++ src/main/java/com/coderscampus/security/demo/service/JwtService.java
@@ -1,10 +1,18 @@
 package com.coderscampus.security.demo.service;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 public class JwtService {
     
@@ -18,11 +26,25 @@ public class JwtService {
      */
     @Value("${jwt.signingKey}")
     private String jwtSigningKey;
+    @Value("${jwt.expirationTimeInMillis}")
+    private Long expirationTimeInMillis;
     
-    public String generateToken(UserDetails user) {
-        Jwts.builder()
-            .setClaims(null)
-            .signWith(null, SignatureAlgorithm.HS256);
+    public String generateToken(Map<String, Object> extraClaims, UserDetails user) {
+        
+       String jwt = Jwts.builder()
+            .setClaims(extraClaims)
+            .setSubject(user.getUsername())
+            .setIssuedAt(new Date())
+            .setExpiration( new Date(System.currentTimeMillis() + expirationTimeInMillis) )
+            .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+            .compact();
+        return jwt;
+    }
+
+    private Key getSigningKey() {
+        byte[] jwtSigningKeyAsBytes = Decoders.BASE64.decode(jwtSigningKey);
+        SecretKey secretKey = Keys.hmacShaKeyFor(jwtSigningKeyAsBytes);
+        return secretKey;
     }
     
 }
