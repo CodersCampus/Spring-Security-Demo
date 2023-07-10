252e4233bdc7c894c110ceace61b25331f937586
tp02ga <trevor@coderscampus.com>
2023-07-10 13:33:38 -0400

    23 - Creating a Custom Filter for the SecurityFilterChain Part 2

--- src/main/java/com/coderscampus/security/demo/security/JwtAuthenticationFilter.java
+++ src/main/java/com/coderscampus/security/demo/security/JwtAuthenticationFilter.java
@@ -2,9 +2,15 @@ package com.coderscampus.security.demo.security;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.coderscampus.security.demo.service.JwtService;
+import com.coderscampus.security.demo.service.UserService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -14,23 +20,41 @@ import jakarta.servlet.http.HttpServletResponse;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private JwtService jwtService;
+    private UserService userService;
     
+    public JwtAuthenticationFilter(JwtService jwtService, UserService userService) {
+        super();
+        this.jwtService = jwtService;
+        this.userService = userService;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         // Requests: 
         //  Headers -> key/value pairs (Authorization -> Bearer xxx.yyy.zzz)
         //  Body -> (if JSON) key/value pairs
-        String auth = request.getHeader("Authorization");
+        String authHeader = request.getHeader("Authorization");
         
-        if (!auth.isEmpty()) {
+        if (!authHeader.isEmpty()) {
             // hey, we have a token (probably) in the request
             // let's see if this token is a valid JWS or not
+            String token = authHeader.substring(7);
+            String subject = jwtService.getSubject(token);
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
             
-            
-            // jwtService.getSubject(auth);
-            //jwtService.isValidToken(auth, null);
+            if (!subject.isEmpty() && authentication == null) {
+                UserDetails userDetails = userService.loadUserByUsername(subject);
+                
+                if (jwtService.isValidToken(token, userDetails)) {
+                    SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword());
+                    securityContext.setAuthentication(authToken);
+                    SecurityContextHolder.setContext(securityContext);
+                } 
+            }
         }
+        filterChain.doFilter(request, response);
     }
 
 }
